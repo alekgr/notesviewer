c5e3aaa7815fccdf38250c0d4ae038c9d6cc3fe9
alek grigorian <alek.grigorian@gmail.com>
2018-03-17 18:34:43 -0500

    added cm_delete() and directoryempty() for delete functions

--- core.py
+++ core.py
@@ -4,6 +4,7 @@ import os
 import pprint
 import re
 import sys
+import shutil
 import vardata 
 from config import *
 
@@ -88,8 +89,41 @@ def cm_add(name, scope, verbose):
 	else:
 		print(name+" already exists")
 		return False
-	
 
+def cm_delete(name, scope, verbose):
+	
+	#delete  a note or catagory
+
+	path = vardata.base_catagory_path+"/"+name
+
+	if not os.path.exists(path):
+		print(name+" does not exist")
+		return False
+		
+	elif os.path.exists(path) == True:
+		if scope == "note":
+			if not os.path.isfile(path):
+				print(name+" "+"does not seem to be a note")
+				return False
+				
+			prompt = raw_input("Are you sure you want to delete "+name+" (yes/no) ")	
+			if prompt == "yes":
+				os.remove(path)
+				return True
+			else:
+				return False 
+		if scope == "catagory":
+			if not os.path.isdir(path):
+				print(name+" "+"does not seem to be a catagory")
+				return False
+			if not directoryempty(path):
+				prompt = raw_input("Are you sure you want to delete "+name+" catagory and any notes and directories (yes/no) ")
+				if prompt == 'yes':
+					shutil.rmtree(path)
+			else:
+				prompt = raw_input("Are you sure you want to delete "+name+" (yes/no) ")
+				os.rmdir(path)
+				
 def cm_list(scope, verbose):
 
 	if scope == 'catagory':
@@ -110,3 +144,10 @@ def cm_showconfig():
 def cm_setdefaultconfig():
 
 	setdefaultconfig() 
+
+def directoryempty(path):
+	if len(os.listdir(path))>0:
+		return False
+	else:
+		return True
+
