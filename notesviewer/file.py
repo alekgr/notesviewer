a02d8e1c05334a30ab1786466977d58d5cf4c780
alek grigorian <alek.grigorian@gmail.com>
2019-03-02 23:10:08 -0600

    refactoring file.py for pep8

--- notesviewer/file.py
+++ notesviewer/file.py
@@ -48,16 +48,22 @@ def create_config_file():
 
     mode = 0o600 | stat.S_IRUSR
 
-    if  not os.path.exists(notesviewer.vardata.HOME+"/"+"."+notesviewer.vardata.PROGRAM_NAME):
+    config_path = (notesviewer.vardata.HOME + "/" + "." +
+                   notesviewer.vardata.PROGRAM_NAME)
+
+    if not os.path.exists(config_path):
         os.makedirs(notesviewer.vardata.CONFIG_FILE_PATH, exist_ok=True)
 
     error = os.mknod(notesviewer.vardata.CONFIG_FILE_PATH, mode)
     if error is not None:
-        print_err_msg("There was error creating config file "+ notesviewer.vardata.CONFIG_FILE_PATH)
+        print_err_msg("There was error creating config file " +
+                      notesviewer.vardata.CONFIG_FILE_PATH)
         exit(-1)
     else:
-        print_info_msg("Created config file at "+notesviewer.vardata.CONFIG_FILE_PATH)
+        print_info_msg("Created config file at " +
+                       notesviewer.vardata.CONFIG_FILE_PATH)
         notesviewer.commands.setdefaultconfig()
+
 
 def create_notes_root_path():
     """ creat a note root directory """
@@ -65,25 +71,26 @@ def create_notes_root_path():
     mode = 0o755 | stat.S_IRUSR
 
     try:
-        os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH+"/"+"meta")
-        os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH+"/"+"content")
-        os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH+"/"+"tags")
-        os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH+"/"+"link")
+        os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH + "/" + "meta")
+        os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH + "/" + "content")
+        os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH + "/" + "tags")
+        os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH + "/" + "link")
         notesviewer.file.print_info_msg("initalized Root note directory...")
     except OSError:
         notesviewer.file.print_info_msg("Note directory already exists")
         prompt_msg1 = "Would you like to initalize it again(yes/no)\n"
         prompt_msg2 = "This will delete all your notes: "
 
-        prompt = input(prompt_msg1+prompt_msg2)
+        prompt = input(prompt_msg1 + prompt_msg2)
         prompt = prompt.lower()
         if prompt == 'yes':
             shutil.rmtree(notesviewer.vardata.NOTES_ROOT_PATH)
-            os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH+"/"+"meta")
-            os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH+"/"+"content")
-            os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH+"/"+"tags")
-            os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH+"/"+"link")
+            os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH + "/" + "meta")
+            os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH + "/" + "content")
+            os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH + "/" + "tags")
+            os.makedirs(notesviewer.vardata.NOTES_ROOT_PATH + "/" + "link")
             notesviewer.file.print_info_msg("Notes directory re-initalized")
+
 
 def verify_note(note, file_context):
     """ check note """
@@ -91,6 +98,7 @@ def verify_note(note, file_context):
     if os.path.exists(path):
         return True
     return False
+
 
 def verify_notes_root_path():
     """ check NOTES_ROOT_PATH folder exists"""
@@ -102,6 +110,7 @@ def verify_notes_root_path():
         notesviewer.file.print_err_msg(no_root_msg)
         notesviewer.file.print_info_msg(run_init_msg)
         exit(notesviewer.error.ERROR_NO_ROOT_NOTE)
+
 
 def verify_empty_note(note, file_context):
     """ check note if empty """
